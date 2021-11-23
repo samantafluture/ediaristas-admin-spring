5c4e0872a26623c1b79260ad15701f69fb440c56
Samanta Fluture <samantafluture@gmail.com>
2021-11-23 18:58:55 -0200

    create servico table in db with servico class

--- README.md
+++ README.md
@@ -2,12 +2,20 @@
 
 Construído com: Java 17, Spring Boot, Thymeleaf, Maven, Lombok, JPA, Hibernate, MySql, AdminETL.
 
-## Classe Servico.java
+## Classes 
+
+### Servico.java
 - classe `POJO` tradicional
 - sem lógica de negócio, apenas argumentos
 - informar que é uma tabela de dados `@Entity`
 - usar `LOMBOK` para criar getters, setters etc
 - métodos `equals`, `hashcode` e `toString` devem usar apenas o `id` da classe
 - por isso temos annotations diferentes que incluem apenas o `id`
+- o `id` terá a annotation de `@GeneratedValue` com estratégia de `identity` pois será gerado automaticamente pelo bando de dados
 
+`mvn spring-boot:run` vai criar a tabela `servico` no banco de dados `ediaristas` conectado à aplicação
 
+## Interfaces
+
+### ServicoRepository.java
+- estende a interface `JpaRepository` e dita os tipos `<T, ID>`, sendo `T` o model do tipo `Servico` e o `id` do tipo `Long`, fincando: `<Servico, Long>`
--- src/main/java/br/com/treinaweb/ediaristas/core/models/Servico.java
+++ src/main/java/br/com/treinaweb/ediaristas/core/models/Servico.java
@@ -2,7 +2,13 @@ package br.com.treinaweb.ediaristas.core.models;
 
 import java.math.BigDecimal;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 
 import br.com.treinaweb.ediaristas.core.enums.Icone;
 import lombok.AllArgsConstructor;
@@ -21,42 +27,63 @@ public class Servico {
 
     @EqualsAndHashCode.Include
     @ToString.Include
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @Column(length = 50, nullable = false)
     private String nome;
 
+    @Column(name = "valor_minimo", nullable = false)
     private BigDecimal valorMinimo;
 
+    @Column(name = "qto_horas", nullable = false)
     private Integer qtdHoras;
 
+    @Column(name = "porcentagem_comissao", nullable = false)
     private BigDecimal porcentagemComissao;
 
+    @Column(name = "horas_quarto", nullable = false)
     private Integer horasQuarto;
 
+    @Column(name = "valor_quarto", nullable = false)
     private BigDecimal valorQuarto;
 
+    @Column(name = "horas_sala", nullable = false)
     private Integer horasSala;
 
+    @Column(name = "valor_sala", nullable = false)
     private BigDecimal valorSala;
 
+    @Column(name = "horas_banheiro", nullable = false)
     private Integer horasBanheiro;
 
+    @Column(name = "valor_banheiro", nullable = false)
     private BigDecimal valorBanheiro;
 
+    @Column(name = "horas_cozinha", nullable = false)
     private Integer horasCozinha;
 
+    @Column(name = "valor_cozinha", nullable = false)
     private BigDecimal valorCozinha;
 
-    private Integer horasQuintas;
+    @Column(name = "horas_quintal", nullable = false)
+    private Integer horasQuintal;
 
-    private BigDecimal valorQuintas;
+    @Column(name = "valor_quintal", nullable = false)
+    private BigDecimal valorQuintal;
 
+    @Column(name = "horas_outros", nullable = false)
     private Integer horasOutros;
 
+    @Column(name = "valor_outros", nullable = false)
     private BigDecimal valorOutros;
 
+    @Column(nullable = false, length = 14)
+    @Enumerated(EnumType.STRING)
     private Icone icone;
 
+    @Column(nullable = false)
     private Integer posicao;
 
 }
