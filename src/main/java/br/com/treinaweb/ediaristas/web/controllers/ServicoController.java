ed059e5ba68449957214156292df5084f85575bf
Samanta Fluture <samantafluture@gmail.com>
2021-11-24 09:42:03 -0200

    create action to insert data to table servico

--- src/main/java/br/com/treinaweb/ediaristas/web/controllers/ServicoController.java
+++ src/main/java/br/com/treinaweb/ediaristas/web/controllers/ServicoController.java
@@ -1,24 +1,38 @@
 package br.com.treinaweb.ediaristas.web.controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
 import br.com.treinaweb.ediaristas.core.enums.Icone;
 import br.com.treinaweb.ediaristas.core.models.Servico;
+import br.com.treinaweb.ediaristas.core.repositories.ServicoRepository;
 
 @Controller
 @RequestMapping("/admin/servicos")
 public class ServicoController {
 
-    @RequestMapping("/cadastrar")
+    @Autowired
+    private ServicoRepository repository;
+
+    @GetMapping("/cadastrar")
     public ModelAndView cadastrar() {
         var modelAndView = new ModelAndView("admin/servico/form");
 
         modelAndView.addObject("servico", new Servico());
 
         return modelAndView;
+    }
+
+    @PostMapping("/cadastrar")
+    public String cadastrar(Servico servico) {
+        repository.save(servico);
+
+        return "redirect:/admin/servicos/cadastrar";
     }
 
     // icons available to all views as an attribute
