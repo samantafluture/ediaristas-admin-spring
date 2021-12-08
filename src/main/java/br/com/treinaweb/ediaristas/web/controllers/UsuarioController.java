afc473d11df65d9d49f83c3eaed488987354501d
Samanta Fluture <samantafluture@gmail.com>
2021-12-08 10:41:49 -0200

    create user list

---
+++ src/main/java/br/com/treinaweb/ediaristas/web/controllers/UsuarioController.java
@@ -0,0 +1,26 @@
+package br.com.treinaweb.ediaristas.web.controllers;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+import br.com.treinaweb.ediaristas.web.services.WebServiceUsuario;
+
+@Controller
+@RequestMapping("/admin/usuarios")
+public class UsuarioController {
+
+    @Autowired
+    private WebServiceUsuario service;
+    
+    @GetMapping
+    public ModelAndView buscarTodos() {
+        var modelAndView = new ModelAndView("admin/usuario/lista");
+
+        modelAndView.addObject("usuarios", service.buscarTodos());
+
+        return modelAndView;
+    }
+}
