30fcc96f87eeac7d966bf12292bd9da5d48098d4
Samanta Fluture <samantafluture@gmail.com>
2021-12-08 11:55:06 -0200

    create action to add user to the database

--- src/main/java/br/com/treinaweb/ediaristas/web/controllers/UsuarioController.java
+++ src/main/java/br/com/treinaweb/ediaristas/web/controllers/UsuarioController.java
@@ -1,11 +1,18 @@
 package br.com.treinaweb.ediaristas.web.controllers;
 
+import javax.validation.Valid;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
+import br.com.treinaweb.ediaristas.web.dtos.FlashMessage;
 import br.com.treinaweb.ediaristas.web.dtos.UsuarioCadastroForm;
 import br.com.treinaweb.ediaristas.web.services.WebServiceUsuario;
 
@@ -15,7 +22,7 @@ public class UsuarioController {
 
     @Autowired
     private WebServiceUsuario service;
-    
+
     @GetMapping
     public ModelAndView buscarTodos() {
         var modelAndView = new ModelAndView("admin/usuario/lista");
@@ -32,5 +39,20 @@ public class UsuarioController {
         modelAndView.addObject("cadastroForm", new UsuarioCadastroForm());
 
         return modelAndView;
+    }
+
+    @PostMapping("/cadastrar")
+    public String cadastrar(
+            @Valid @ModelAttribute("cadastroForm") UsuarioCadastroForm cadastroForm,
+            BindingResult result,
+            RedirectAttributes attrs) {
+        if (result.hasErrors()) {
+            return "admin/usuario/cadastro-form";
+        }
+
+        service.cadastrar(cadastroForm);
+        attrs.addFlashAttribute("alert", new FlashMessage("alert-success", "Usuário cadastrado com sucesso!"));
+
+        return "redirect:/admin/usuarios";
     }
 }
--- src/main/java/br/com/treinaweb/ediaristas/web/services/WebServiceUsuario.java
+++ src/main/java/br/com/treinaweb/ediaristas/web/services/WebServiceUsuario.java
@@ -5,8 +5,11 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import br.com.treinaweb.ediaristas.core.enums.TipoUsuario;
 import br.com.treinaweb.ediaristas.core.models.Usuario;
 import br.com.treinaweb.ediaristas.core.repositories.UsuarioRepository;
+import br.com.treinaweb.ediaristas.web.dtos.UsuarioCadastroForm;
+import br.com.treinaweb.ediaristas.web.mappers.WebUsuarioMapper;
 
 @Service
 public class WebServiceUsuario {
@@ -14,7 +17,19 @@ public class WebServiceUsuario {
     @Autowired
     private UsuarioRepository repository;
 
+    @Autowired
+    private WebUsuarioMapper mapper;    
+
     public List<Usuario> buscarTodos() {
         return repository.findAll();
+    }
+
+    public Usuario cadastrar(UsuarioCadastroForm form) {
+        var model = mapper.toModel(form);
+
+        // all users created by default are admin users
+        model.setTipoUsuario(TipoUsuario.ADMIN);
+
+        return repository.save(model);
     }
 }
